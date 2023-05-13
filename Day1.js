630d1d5da8ca939077801d1b71ea94b80bfb54ff
Ajay Chaudhari <81674825+archaudhari@users.noreply.github.com>
2023-05-13 22:01:59 +0530

    1.d

--- Day1.js
+++ Day1.js
@@ -19,3 +19,40 @@ function filterByLength(arr, num) {
   }
   return result;
 }
+//1.c
+function sumOfPrimes(num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+//1.d
+function longestIncreasingSubsequence(arr) {
+  const n = arr.length;
+  const dp = new Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+}

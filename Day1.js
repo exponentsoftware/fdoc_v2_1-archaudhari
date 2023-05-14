4c46b7ea9e519284739054d31a50f4a8d2fc437a
Ajay Chaudhari <81674825+archaudhari@users.noreply.github.com>
2023-05-14 09:46:12 +0530

    2.b

--- Day1.js
+++ Day1.js
@@ -69,3 +69,14 @@ function findPairs(arr, target) {
   }
   return pairs;
 }
+//2.b
+var a = [1, 2, 3, 5, 6, 7, 8, 9, 10],
+  count = 5;
+var missing = new Array();
+
+for (var i = 1; i <= count; i++) {
+  if (a.indexOf(i) == -1) {
+    missing.push(i);
+  }
+}
+console.log(missing);

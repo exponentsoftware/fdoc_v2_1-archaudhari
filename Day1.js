9d545bfe27540e3023ed45372f85e0c403247bf6
Ajay Chaudhari <81674825+archaudhari@users.noreply.github.com>
2023-05-14 10:59:14 +0530

    2.d

--- Day1.js
+++ Day1.js
@@ -86,3 +86,14 @@ function findNthLargest(arr, n) {
   return sorted[n - 1];
 }
 //2.d
+function findMaximumSubarray(arr) {
+  let maxSum = arr[0];
+  let currentSum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    currentSum = Math.max(currentSum + arr[i], arr[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
